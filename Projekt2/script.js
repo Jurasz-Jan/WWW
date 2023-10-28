7504fd967062c8d033457d4b5053a7436d0dd182
Jan Jurasz <101977860+Jurasz-Jan@users.noreply.github.com>
2023-10-28 19:43:49 +0200

    added getCorrectCount()

--- Projekt2/script.js
+++ Projekt2/script.js
@@ -32,11 +32,99 @@ const pytania = `{
         }
 
 
+        {
+            "tekst": "Co jest to element HTML?",
+            "odpowiedzi": [
+              "Jednostka składowa dokumentu HTML",
+              "Zbiór elementów HTML",
+              "Obrazek na stronie HTML",
+              "Tekst na stronie HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to atrybut HTML?",
+            "odpowiedzi": [
+              "Opis właściwości elementu HTML",
+              "Jednostka składowa dokumentu HTML",
+              "Zbiór elementów HTML",
+              "Obrazek na stronie HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to tag HTML?",
+            "odpowiedzi":
+            [
+              "Jednostka składowa dokumentu HTML",
+              "Opis właściwości elementu HTML",
+              "Zbiór elementów HTML",
+              "Obrazek na stronie HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to selektor CSS?",
+            "odpowiedzi":
+            [
+              "Wyrażenie określające, które elementy CSS mają zostać stylizowane",
+              "Jednostka składowa dokumentu HTML",
+              "Opis właściwości elementu HTML",
+              "Zbiór elementów HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to właściwość CSS?",
+            "odpowiedzi":
+            [
+              "Wartość przypisana do selektora CSS",
+              "Jednostka składowa dokumentu HTML",
+              "Opis właściwości elementu HTML",
+              "Zbiór elementów HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to wartość CSS?",
+            "odpowiedzi":
+            [
+              "Wartość przypisana do właściwości CSS",
+              "Jednostka składowa dokumentu HTML",
+              "Opis właściwości elementu HTML",
+              "Zbiór elementów HTML"
+            ],
+            "poprawna": [1]
+          },
+          {
+            "tekst": "Co jest to protokół HTTP?",
+            "odpowiedzi":
+            [
+              "Protokół przesyłania plików",
+              "Protokół przesyłania danych",
+              "Protokół przesyłania dokumentów",
+              "Protokół przesyłania stron internetowych"
+            ],
+            "poprawna": [4]
+          },
+          {
+            "tekst": "Co jest to domena internetowa?",
+            "odpowiedzi":
+            [
+              "Identyfikator komputera w sieci",
+              "Identyfikator strony internetowej",
+              "Nazwa serwera WWW",
+              "Adres strony internetowej"
+            ],
+            "poprawna": [3]
+          },
+
+
     ],
 }`
 const nrPytania = 0
 
-function nextQuestion() {
+function nextQuestion() {  // co to robi? po co? ~Janekm
     const obj = JSON.parse(pytania)
     if (nrPytania >= obj.pytania.length) {
         checkAnswers()
@@ -51,7 +139,15 @@ function checkAnswers() {
     const answers = {
         q1: 'b', // Poprawna odpowiedź dla pytania 1
         q2: 'a',
-        q3: 'a'
+        q3: 'a',
+        q4: 'a',
+        q5: 'a',
+        q6: 'a',
+        q7: 'a',
+        q8: 'a',
+        q9: 'a',
+        q10: 'd',
+        q11: 'c'
     };
 
     let correctCount = 0
@@ -66,4 +162,10 @@ function checkAnswers() {
 
     const results = document.getElementById("results")
     results.innerHTML = `Liczba poprawnych odpowiedzi: ${correctCount}/20`
+}
+
+
+function getCorrectCount()
+{
+    return correctCount;
 }
